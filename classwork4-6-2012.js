6b87d1b1f39c98a3406d0b4f3887f3810f5204c3
Dario Pasquantonio <dario@Dario-Mac.local>
2012-07-16 21:50:28 +0200

    ciao

--- classwork4-6-2012.js
+++ classwork4-6-2012.js
@@ -1635,18 +1635,41 @@ var grass4 = SIMPLEX_GRID([[-9/23*xGarden1,5/23*xGarden1],[-0.01,0.01],[11/23*xG
 grass4 = COLOR([0/255, 153/255, 0/255])(grass4);
 grass4 = T([2])([-11/23*xGarden1])(grass4);
 garden.push(grass4);
+var pointsStreet4 = [[16/23*xGarden1,0.01,-11/23*xGarden1],[16/23*xGarden1,0.01,-13/23*xGarden1],[13/23*xGarden1,0.01,-16/23*xGarden1],[10/23*xGarden1,0.01,-16/23*xGarden1],[7/23*xGarden1,0.01,-13/23*xGarden1],[7/23*xGarden1,0.01,-11/23*xGarden1]];
+var profileStreets4 = NUBS(S0)(2)(makeKnots(pointsStreet4))(pointsStreet4);
+
+var pointsStreet5 = [[14/23*xGarden1,0.01,-11/23*xGarden1],[14/23*xGarden1,0.01,-13/23*xGarden1],[11.5/23*xGarden1,0.01,-13/23*xGarden1],[9/23*xGarden1,0.01,-13/23*xGarden1],[9/23*xGarden1,0.01,-11/23*xGarden1]];
+var profileStreets5 = NUBS(S0)(2)(makeKnots(pointsStreet5))(pointsStreet5);
+var streets4 = BEZIER(S1)([profileStreets4,profileStreets5]);
+streets4 = MAP(streets4)(domain2d);
+garden.push(streets4);
+
+var pointsGrass5 = [[16/23*xGarden1,0,-16/23*xGarden1],[11.5/23*xGarden1,0,-16/23*xGarden1],[7/23*xGarden1,0,-16/23*xGarden1]];
+var nubsGrass5 = NUBS(S0)(2)(makeKnots(pointsGrass5))(pointsGrass5);
+var grass5 = BEZIER(S1)([nubsGrass5,profileStreets4]);
+grass5 = MAP(grass5)(domain2d);
+grass5 = COLOR([0/255, 153/255, 0/255])(grass5);
+garden.push(grass5); 
+
+var pointsGrass6 = [[14/23*xGarden1,0,-11/23*xGarden1],[11.5/23*xGarden1,0,-11/23*xGarden1],[9/23*xGarden1,0,-11/23*xGarden1]];
+var nubsGrass6 = NUBS(S0)(2)(makeKnots(pointsGrass6))(pointsGrass6);
+var grass6 = BEZIER(S1)([nubsGrass6,profileStreets5]);
+grass6 = MAP(grass6)(domain2d);
+grass6 = COLOR([0/255, 153/255, 0/255])(grass6);
+garden.push(grass6); 
+
 garden = STRUCT(garden);
 garden = T([0,1,2])([-12,-0.02,-2])(garden);
 return garden;
 }
 
 var ville = [];
-//var facades = buildFacades();
-//ville.push(facades);
+var facades = buildFacades();
+ville.push(facades);
 var walls = buildWalls();
 ville.push(walls);
-//var roof = buildRoof();
-//ville.push(roof);
+var roof = buildRoof();
+ville.push(roof);
 var garden = buildGarden();
 ville.push(garden);
 ville = STRUCT(ville);
